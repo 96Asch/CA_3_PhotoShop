3e5cce029f1398b360c7baf5331cab954439104c
A.S. Huang <s1913999@duranium.liacs.nl>
2017-12-21 22:21:58 +0100

    begin cuda

--- filters-cuda.cu
+++ filters-cuda.cu
@@ -38,6 +38,60 @@ cudaAssert(cudaError_t code, const char *file, int line, bool abort=true)
  */
 
 __global__ void
+filters_cuda_apply_gamma(uint32_t *src, const int rowstride, const int x, const int y, const int width, const int height)
+{
+  const int xx = blockIdx.x * blockDim.x + threadIdx.x;
+  const int yy = blockIdx.y * blockDim.y + threadIdx.y;
+  
+   if (xx < x || xx >= width || yy < y || y >= height)
+    return;
+
+  uint32_t pixel = *image_get_pixel_data(src, rowstride, xx, yy);
+         /* Copy alpha value */
+          uint32_t result = pixel & 0xFF;
+
+          /* Gamma correct each color component using bit shifts (see gamma.h) */
+          result |= GAMMA[0xFF & (pixel >> 24)] << 24; // red
+          result |= GAMMA[0xFF & (pixel >> 16)] << 16; // green
+          result |= GAMMA[0xFF & (pixel >> 8)] << 8;   // blue
+  *image_get_pixel_data(src, rowstride, xx, yy) = result;
+}
+
+__device__ void
+vec_mults(float* src, const float scalar, const int size) 
+{
+ const int index = blockIdx.x * blockDim.x + threadIdx.x;
+ if(index >= size)
+    return;
+
+ src[index] *= scalar;
+}
+
+__global__ void
+filters_cuda_brightness(uint32_t *src, const int rowstride, const int x, const int y, const int width, const int height, const float mult, float* temp)
+{
+  const int xx = blockIdx.x * blockDim.x + threadIdx.x;
+  const int yy = blockIdx.y * blockDim.y + threadIdx.y;
+
+   if (xx < x || xx >= width || yy < y || y >= height)
+    return;
+
+   rgba_t color;
+   RGBA_unpack(color, *image_get_pixel_data(src, rowstride, xx, yy));
+   temp[0] = color.x;
+   temp[1] = color.y;
+   temp[2] = color.z;
+   temp[3] = color.w;
+   int size = 4;
+   vec_mults(temp, mult, size);
+   color.x = temp[0];
+   color.y = temp[1];
+   color.z = temp[2];
+   color.w = temp[3];
+   RGBA_pack(*image_get_pixel_data(src, rowstride, xx, yy), color);      
+}
+
+__global__ void
 filters_cuda_copy(uint32_t *dst, const uint32_t *src, const int rowstride,
                   const int x, const int y,
                   const int width, const int height)
@@ -76,34 +130,40 @@ run_filters(image_t *result, const image_t *image, const int n_repeat)
                           image->height / blocksize.y + 1 :
                           image->height / blocksize.y);
 
-  /* TODO: Allocate buffers to contain initial and result images */
-
-  /* Copy the input image to the initial buffer */
-
+  uint32_t *result_D;
+  uint32_t *image_D;
+  cudaMalloc(&result_D, sizeof(uint32_t) * image->width);
+  cudaMalloc(&image_D, sizeof(uint32_t) * image->width);
+  cudaMemcpy(image_D, &image->data, sizeof(uint32_t) * image->width, cudaMemcpyHostToDevice);
   /* Start the timer */
   CUDA_ASSERT(cudaEventRecord(start));
-
+  float *temp;
+  cudaMalloc(&temp, sizeof(float) * 4);
   for(int i = 0; i < n_repeat; i++)
     {
       /* Copy the initial buffer to the result buffer. This is similar
        * to the operation done in the CPU code to allow a fair comparison.
        */
-/* Example CUDA kernel call:
+
       filters_cuda_copy<<<numblocks, blocksize>>>(result_D, image_D,
                                                   image->rowstride,
                                                   0, 0,
                                                   image->width,
                                                   image->height);
+      filters_cuda_apply_gamma<<<numblocks, blocksize>>>(result_D, image->rowstride, 0, 0, image->width, image->height);
+      filters_cuda_brightness<<<numblocks, blocksize>>>(result_D, image->rowstride, 0, 0, image->width, image->height,0.8, temp);
       CUDA_ASSERT( cudaGetLastError() );
-*/
+
 
     }
 
   /* Pipeline is finished */
   CUDA_ASSERT(cudaEventRecord(stop));
 
-  /* TODO: Copy the result buffer back to result->data */
-
+  cudaMemcpy(result->data, result_D, sizeof(uint32_t), cudaMemcpyDeviceToHost);
+  cudaFree(result_D);
+  cudaFree(image_D);
+  cudaFree(temp);
   /* Synchronize and print statistics */
   CUDA_ASSERT(cudaEventSynchronize(stop ));
   float msec = 0;
